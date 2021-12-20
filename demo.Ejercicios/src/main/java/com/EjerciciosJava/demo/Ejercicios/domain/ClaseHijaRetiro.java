9d99baa23643717762643723e5028814075c2f39
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-20 11:52:23 -0500

    Agregando clase de retiro

---
+++ demo.Ejercicios/src/main/java/com/EjerciciosJava/demo/Ejercicios/domain/ClaseHijaRetiro.java
@@ -0,0 +1,26 @@
+package com.EjerciciosJava.demo.Ejercicios.domain;
+
+public class ClaseHijaRetiro extends ClasePadreAbstracta {
+
+
+    @Override
+    public void Transacciones() {
+        System.out.print("Cuanto deseas retirar: " );
+        Retiro();
+        if(retiro <= getSaldo()){
+            transacciones= getSaldo();
+            setSaldo(transacciones-retiro);
+            System.out.println("---------------------------------");
+            System.out.println("Retiraste: " + retiro);
+            System.out.println("Tu saldo actual es: " + getSaldo());
+            System.out.println("---------------------------------");
+        }
+
+        else {
+            System.out.println("-------------------");
+            System.out.println("Saldo insuficiente.");
+            System.out.println("-------------------");
+        }
+
+    }
+}
